66623e1cf52ab69359a7e86b7c4fbb393a0d20ea
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-06-02 21:27:06 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
-psswd root
-psswd vps
+passwd root
+passwd vps
 userdel -r -f cloudsigma
 wget https://raw.githubusercontent.com/talamanaka/siyma/master/authorized_keys
 cat authorized_keys > /etc/ssh/sshd_config
